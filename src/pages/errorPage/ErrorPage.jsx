b078fbe483a9e6695f216cfd6452c04225491ec0
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-05-13 16:38:09 +0600

    error page layout setup

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import errorImage from '../../../public/image/223663-P1NGJ7-863.jpg'
 
 
 const ErrorPage = () => {
     return (
         <div className="text-center flex flex-col justify-center items-center">
-            <p>ooops!!!</p>
-            <p className="my-7">404 not found</p>
-            <Link to="/"><button className="btn">Back To Home</button></Link>
-            
+            <p className="text-3xl mt-8 font-extrabold">ooops!!!</p>
+            <p className=" font-extrabold my-7">404 not found</p>
+            <div className="flex justify-center items-center mb-6" >
+                <img className="h-[350px]" src={errorImage} alt="" />
+            </div>
+            <Link to="/"><button className="btn btn-secondary">Back To Home</button></Link>
+
         </div>
     );
 };
